935990ede2805ba7e6cdf2d96a673e5869aa1ac6
Joshua Duggan <joshua.duggan@gmail.com>
2018-06-28 14:30:00 -0400

    Added smooth follow camera

---
+++ assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform heroTrans;
+    public Vector3 offset;
+    public float smoothSpeedX;
+    public float smoothSpeedY;
+
+
+    void FixedUpdate () {
+        transform.position = new Vector3(
+                Mathf.Lerp(transform.position.x, heroTrans.position.x + offset.x, smoothSpeedX),
+                Mathf.Lerp(transform.position.y, heroTrans.position.y + offset.y, smoothSpeedY),
+                offset.z);
+    }
+}
